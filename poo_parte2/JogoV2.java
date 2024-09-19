b351262a2f33f100f41a3182d4e98a9f80e805cf
Renato Silva <Renatohorikoshi@gmail.com>
2024-09-19 17:49:21 -0300

    feat: mais funcoes para o jogo

--- poo_parte2/JogoV2.java
+++ poo_parte2/JogoV2.java
@@ -1,16 +1,66 @@
+import java.util.Random;
 public class JogoV2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         
+        System.out.println("Começando..");
         var bonecudo = new Personagem();
+        var soneca = new Personagem(2,4,10);
+        var fominha = new Personagem(6,10,2);
         bonecudo.nome = "H";
+        soneca.nome = "Pooh";
+       fominha.nome = "Scooby";
+       var gerador = new Random();
+       
         while (true){
-            bonecudo.cacar();
-            bonecudo.cacar();
-        }
-        }
-        
+            
+
+            
+            switch (gerador.nextInt(3)+1){
+                case 1:
+                bonecudo.cacar();
+                break;
+                case 2:
+                bonecudo.comer();
+                break;
+                case 3:
+                bonecudo.dormir();
+                break;
+                }
+                System.out.println(bonecudo);
+            
+                System.out.println("----------------");
+                switch (gerador.nextInt(3)+1){
+                    case 1:
+                    soneca.cacar();
+                    break;
+                    case 2:
+                    soneca.comer();
+                    break;
+                    case 3:
+                    soneca.dormir();
+                    break;
+                }
+            System.out.println(soneca);
+                System.out.println("----------------");
+                switch (gerador.nextInt(3)+1){
+                    case 1:
+                    fominha.cacar();
+                    break;
+                    case 2:
+                    fominha.comer();
+                    break;
+                    case 3:
+                    fominha.dormir();
+                    break;
+                }
+                System.out.println(fominha);
+                System.out.println("=================");
+                Thread.sleep(6000);
+            }
             
         }
+    }
+
         
 
     
--- poo_parte2/Personagem.java
+++ poo_parte2/Personagem.java
@@ -1,19 +1,31 @@
 
-
 public class Personagem{
-
     String nome;
-    int energia = 10;
-    int fome = 0;
-    int sono = 0;
-    
+    private int energia;
+    private int fome;
+    private int sono;
+
+    Personagem(){
+        System.out.println("Construindo novo personagem");
+        energia = 10;
+        fome = 0;
+        sono = 0;
+    }
+
+    Personagem(int energia, int fome, int sono){
+        System.out.println("Construindo novo personagem");
+        this.energia = energia< 0|| energia > 10 ? 10:energia;
+        this.fome = fome >=0 && fome <=10 ? fome : 0;
+        this.sono = sono>=0 && sono <= 10 ? sono : 0;
+}
     void cacar(){
         if(energia >=2){
         System.out.printf(nome +" caçando\n");
         energia-=2;
+        
     }else{
         System.out.printf(
-            "%s sem energia para cacar"
+            "%s sem energia para cacar\n"
             ,nome
         );
     }
@@ -21,12 +33,12 @@ public class Personagem{
     fome = fome == 10 ? fome : fome + 1;
     //sono = sono +1 > 10 ? sono : sono + 1;
     sono = sono == 10 ? sono : sono + 1;
-    }
+}
 
     void comer(){
         
-        System.out.printf(nome +" comendo\n");
         if(fome >=1){
+            System.out.printf(nome +" comendo\n");
             energia = Math.min(energia + 1, 10);
             //energia = energia == 10 ? energia : energia + 1;
             fome--;
@@ -36,18 +48,22 @@ public class Personagem{
         
     }
     void dormir(){
-        System.out.printf(nome+ " dormindo");
         if (sono >= 1){
+            System.out.printf(nome+ " dormindo\n");
             energia = Math.min(energia + 1 , 10);
         //energia = energia == 10 ? energia : energia +1;
             sono--;
     }else{
         System.out.println(nome +" sem sono");
     }
+}
+        public String toString(){
+
+            return String.format("%s: e:%d, f:%d, s:%d",nome,energia,fome,sono);
+
         
     }
-
-
 }
+
 
 
