382b9a487e75d76f16894f52fb35fb9bf8a3fd2b
Renato Horikoshi <Renatohorikoshi@gmail.com>
2024-10-31 16:30:00 -0300

    Livro de Notas

--- poo_parte1/TesteLivroDeNotas.java
+++ poo_parte1/TesteLivroDeNotas.java
@@ -3,16 +3,16 @@ public class TesteLivroDeNotas{
     static public void main(String...args){
         //construir um livro de notas
         String s= "abc";
-        int comprimento = s.size();
+        int comprimento = s.length();
         
         var disciplina1 = JOptionPane.showInputDialog("Digite o nome da disciplina");
         var meuLivro = new LivroDeNotas();
-        meuLivro.nomeDisciplina = disciplina1;
+        meuLivro.setNomeDisciplina(disciplina1);
         //acionar o comportamento sobre ele
         meuLivro.exibirMensagem();
         var disciplina2 = JOptionPane.showInputDialog("Digite o nome da Disciplina");
         var meuLivro2 = new LivroDeNotas();
-        meuLivro2.nomeDisciplina = disciplina2;
+        meuLivro2.setNomeDisciplina(disciplina2);
         meuLivro2.exibirMensagem();
     }
 }
